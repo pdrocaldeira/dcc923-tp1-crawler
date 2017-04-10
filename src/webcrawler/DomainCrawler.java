826fc52dececf292e6c86627839cf6e1b25d69c1
Pedro Caldeira <pdrocaldeira@gmail.com>
2017-04-10 15:30:31 -0300

    It works but its going to crash your computer because it create a lot of threads and can't kill'em all

--- src/webcrawler/DomainCrawler.java
+++ src/webcrawler/DomainCrawler.java
@@ -10,12 +10,28 @@ public class DomainCrawler extends Thread {
 	private CkSpider spider;
 	private int runs = 0;
 	private double bytes_downloaded = 0;
-	private long creation_time;
+	public final long creation_time;
+	public final String domain;
 	
-	public DomainCrawler(String domain){
+	public static String getDomainFromURL(String url){
+		try{
+			int beginIndex = url.indexOf('.')+1;
+			int endIndex = url.indexOf('/', url.indexOf('.')+1);
+			if(endIndex > 0) return url.substring(beginIndex, endIndex);
+			else return url.substring(beginIndex);
+		}	
+		catch(StringIndexOutOfBoundsException stringIndexOutOfBoundsException){
+			System.out.println("URL ERROR DOMAIN: "+url);
+			stringIndexOutOfBoundsException.printStackTrace();
+			return null;
+		}
+	}
+	
+	public DomainCrawler(String url){
 		this.spider = new CkSpider();
-		this.spider.Initialize(domain);	
+		this.spider.Initialize(url);	
 		this.creation_time = System.currentTimeMillis();
+		this.domain = getDomainFromURL(url);
 	}
 	
 	@Override
@@ -24,8 +40,15 @@ public class DomainCrawler extends Thread {
 		int size, char_count;
 		
 		while(kill == false){
+			
+			long beforeCrawl = System.currentTimeMillis();
 			if(spider.lastHtml().length() != 0 || this.runs == 0) spider.CrawlNext(); // bool return
 			else spider.RecrawlLast();
+			
+			//System.out.println("Time Elapsed to crawl: "+((double)(System.currentTimeMillis()- beforeCrawl))/1000);
+			
+			Main.PAGES_REQUEST++;
+			this.runs++;
 			
 			this.bytes_downloaded += spider.lastHtml().length()*8;
 			
@@ -44,14 +67,21 @@ public class DomainCrawler extends Thread {
 				}				
 			}
 			
-			this.runs++;
+			if(spider.lastHtml().length() > 0) {
+				
+				Main.PAGES_CRAWLED++;
+			}
 			if(spider.get_NumUnspidered() == 0) this.kill = true;
 			else{
 				try {
-					sleep(30000);
+					Main.CRAWLERS_SLEEPING++;
+					int random_extra_time = (int) Math.ceil(Math.random()*10000);
+					sleep(30000+random_extra_time);
+					Main.CRAWLERS_SLEEPING--;
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					
+					//e.printStackTrace();
+					return;
 				}
 			}
 			
@@ -71,5 +101,6 @@ public class DomainCrawler extends Thread {
 	public int getRuns(){
 		return this.runs;
 	}
+	
 
 }
--- src/webcrawler/Main.java
+++ src/webcrawler/Main.java
@@ -4,21 +4,48 @@ import com.chilkatsoft.CkSpider;
 
 import java.util.LinkedList;
 import java.util.concurrent.ConcurrentLinkedQueue;
-
+import java.util.concurrent.PriorityBlockingQueue;
 import java.util.Iterator;	
+import java.util.Comparator;
+import java.util.HashSet;
 
 public class Main {
 	
+	private static final int MAX_SIMULTANEOUS_CRAWLERS = 30; //600 crashed my computer (CAREFUL)
+	private static final int MAX_SIZE_RECENT_DOMAINS = 1000;
+	private static final long START_TIME = System.currentTimeMillis();
+	public static int PAGES_CRAWLED = 0;
+	public static int CRAWLERS_SLEEPING = 0;
+	public static int PAGES_REQUEST = 0;
+	public static int CRAWLERS_KILLED = 0;
+	
+	private final static boolean DEBUG_BANDWIDTH = false;
+	public final static boolean DEBUG_ADD_OUTBOUND_LINK = false;
+	public final static boolean DEBUG_EMPTY_PAGE = false;
+	private final static boolean DEBUG_GLOBAL_QUEUE_SIZE = false;
+	private final static boolean DEBUG_NUMBER_OF_CRAWLERS = false;
+	private final static boolean DEBUG_PAGES_CRAWLED_PER_SECOND = true;
+	private final static boolean DEBUG_PAGES_CRAWLED = false;
+	private final static boolean DEBUG_CRAWLERS_SLEEPING = false;
+	private final static boolean DEBUG_PAGES_REQUEST_PER_SECOND = false;
+	private final static boolean DEBUG_CRAWLERS_KILLED = false;
+	
+		
+	public static RecentDomainList recentDomainList = new RecentDomainList(MAX_SIZE_RECENT_DOMAINS);
+	
 	//TODO: Should have relative max size based on max memory of host machine
 	//See this for reference: http://stackoverflow.com/questions/12807797/java-get-available-memory
-	public static ConcurrentLinkedQueue<String> GLOBAL_QUEUE = new ConcurrentLinkedQueue<String>(); //different domains queue
+	public static PriorityBlockingQueue<String> GLOBAL_QUEUE = new PriorityBlockingQueue<String>(100000000, new Comparator<String>(){
+		public int compare(String url1, String url2){
+			if(recentDomainList.contains(DomainCrawler.getDomainFromURL(url1))){
+				if(recentDomainList.contains(DomainCrawler.getDomainFromURL(url2))) return 0; //they both are recent domains
+				else return 1; //URL2 have priority because it isn't in recent domain
+			}
+			else if(recentDomainList.contains(DomainCrawler.getDomainFromURL(url2))) return -1; //URL1 have priority because it isn't in recent domain
+			return 0; //they both aren't recent domains
+		}
+	}); //different domains queue		
 	
-	private static int MAX_SIMULTANEOUS_CRAWLERS = 300; //600 crashed my computer (CAREFUL)
-	
-	private final static boolean DEBUG_BANDWIDTH = true;
-	public final static boolean DEBUG_ADD_OUTBOUND_LINK = true;
-	public final static boolean DEBUG_EMPTY_PAGE = false;
-	private final static boolean DEBUG_GLOBAL_QUEUE_SIZE = true;
 	
 	static {
 		try {
@@ -36,11 +63,15 @@ public class Main {
 	public static void main(String argv[]){
 		LinkedList<DomainCrawler> crawlers = new LinkedList<DomainCrawler>();
 		
+		long last_debug_time = System.currentTimeMillis();
+		
 		DomainCrawler seed = new DomainCrawler("https://en.wikipedia.org/wiki/Main_Page");
 		
 		DomainCrawler newCrawler, crawler2Die;		
 		
 		crawlers.add(seed);
+		recentDomainList.addLast(seed.domain);		
+		
 		seed.start();
 		
 		while(seed.getRuns() == 0){
@@ -48,6 +79,9 @@ public class Main {
 		}; //Busy Waiting first batch of URLs
 		
 		double allBandwidth;		
+		
+		try{
+			
 		
 		do{
 			allBandwidth = 0;
@@ -57,30 +91,54 @@ public class Main {
 			DomainCrawler auxCrawler;
 			while(crawlerIterator.hasNext()){
 				auxCrawler = crawlerIterator.next();
-				if(auxCrawler.isAlive()) allBandwidth += auxCrawler.getBandwidth();
-				else crawlerIterator.remove();				
+				if(auxCrawler.isAlive()) {
+					allBandwidth += auxCrawler.getBandwidth();					
+				}
+				else {					
+					crawlerIterator.remove();				
+				}
 			}			
 			
 			if(DEBUG_BANDWIDTH) System.out.println("DEBUG: Bandwidth is "+allBandwidth);
 			if(GLOBAL_QUEUE.isEmpty() == false){
 				
-				if(allBandwidth > 1000000/8 || crawlers.size() > MAX_SIMULTANEOUS_CRAWLERS){
+				if( (allBandwidth > 1000000/8 || crawlers.size() >= MAX_SIMULTANEOUS_CRAWLERS) && GLOBAL_QUEUE.size() > 1){					
 					crawler2Die = crawlers.removeFirst();
 					crawler2Die.kill();
+					crawler2Die.interrupt();
+					CRAWLERS_KILLED++;					
 				}
 				
-				newCrawler = new DomainCrawler(GLOBAL_QUEUE.poll());
-				crawlers.add(newCrawler);
-				newCrawler.start();
+				if(crawlers.size() < MAX_SIMULTANEOUS_CRAWLERS){
+					newCrawler = new DomainCrawler(GLOBAL_QUEUE.poll());
+					crawlers.add(newCrawler);
+					recentDomainList.addLast(newCrawler.domain);
+					newCrawler.start();
+				}								
 				
-				if(DEBUG_GLOBAL_QUEUE_SIZE) System.out.println("DEBUG: GLOBAL QUEUE size: "+GLOBAL_QUEUE.size());
-				System.out.println("DEBUG: There are "+crawlers.size()+" threads");
+				if(System.currentTimeMillis() - last_debug_time > 30000){
+					last_debug_time = System.currentTimeMillis();
+					if(DEBUG_GLOBAL_QUEUE_SIZE) System.out.println("DEBUG: GLOBAL QUEUE size: "+GLOBAL_QUEUE.size());
+					if(DEBUG_NUMBER_OF_CRAWLERS) System.out.println("DEBUG: There are "+crawlers.size()+" threads");
+					if(DEBUG_PAGES_CRAWLED_PER_SECOND) System.out.println("DEBUG: Pages Crawled per second: "+((double)PAGES_CRAWLED*1000)/(System.currentTimeMillis() - START_TIME));
+					if(DEBUG_PAGES_CRAWLED) System.out.println("DEBUG: Pages Crawled: "+PAGES_CRAWLED);
+					if(DEBUG_CRAWLERS_SLEEPING) System.out.println("DEBUG: Sleeping Crawlers "+CRAWLERS_SLEEPING);
+					if(DEBUG_PAGES_REQUEST_PER_SECOND) System.out.println("DEBUG: Pages Requested per second: "+((double)PAGES_REQUEST*1000)/(System.currentTimeMillis() - START_TIME));
+					if(DEBUG_CRAWLERS_KILLED) System.out.println("DEBUG: Crawlers Killed: "+CRAWLERS_KILLED);
+				}
 			}
 			
 		
 							
 			
-		}while(!crawlers.isEmpty() || !GLOBAL_QUEUE.isEmpty());
+		}while(!crawlers.isEmpty() || !GLOBAL_QUEUE.isEmpty() || CRAWLERS_SLEEPING > 0);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		catch(OutOfMemoryError oome){
+			System.out.println("OOME");
+			
+		}
 		System.out.println("Terminating "+crawlers.size()+" "+GLOBAL_QUEUE.size());
 	}
 }
